57aafff0bbbee257610432bd23253f0c327a5a1e
Miguel <miguel@digitec.ws>
2019-04-13 13:34:37 -0700

    initial commit

---
+++ src/main/java/com/digitec/factura/FacturaApplication.java
@@ -0,0 +1,13 @@
+package com.digitec.factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digitec/factura/FacturaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digitec.factura;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacturaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
